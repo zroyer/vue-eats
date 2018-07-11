b090f0ae4593807df9263be6e0ebce07e4b501b7
zroyer <zroyer@utexas.edu>
2018-07-11 12:01:45 -0500

    adds header

---
+++ client/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <router-link tag="a" class="navbar-brand" to="/" exact>Vue Eats</router-link>
+    </div>
+  </nav>
+</template>
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -34,5 +34,6 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  modules: ['bootstrap-vue/nuxt']
 }
